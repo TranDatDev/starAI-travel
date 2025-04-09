10e2b73e4d6f5769746334f9d9c0ceabd6b2c2d1
TranDatDev <tranthanhdat11042003@gmail.com>
2025-04-09 17:19:09 +0700

    Attempt in deploying to Railway

--- accommodation-service/src/main.ts
+++ accommodation-service/src/main.ts
@@ -1,26 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import * as process from 'process';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Bật cors
-  const whitelist = ['http://localhost:5173', 'http://192.168.1.71:5173'];
-
-  app.enableCors({
-    origin: (origin, callback) => {
-      if (!origin || whitelist.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
+  app.enableCors();
   app.setGlobalPrefix('api');
   app.enableVersioning({
     type: VersioningType.URI,
   });
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(process.env.PORT || 3001, '0.0.0.0');
 }
 bootstrap();
